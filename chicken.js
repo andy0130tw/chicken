b05e1229115e0b3d1739491ac51e24f2d1d5994e
Andy Pan <andy0130tw@yahoo.com.tw>
2017-01-31 08:42:21 +0800

    import the work I did overnight

---
+++ chicken.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+var chicken = require('./lib/chicken');
+var utils = require('./lib/utils');
+
+if(process.argv[2] !== undefined) {
+  fs.readFile(process.argv[2], 'utf-8', (err, code) => {
+    if(err)
+      throw err;
+    if (!code.length)
+      throw new TypeError('Cannot execute an empty file.');
+
+    let output = chicken(process.argv[3], code);
+
+    if (output[-1])
+      throw new RangeError('Internal error in chicken.js: arbitrary chicken injection detected!');
+
+    if (output !== undefined)
+      console.log(utils.parseHtmlEntities(output));
+  });
+} else {
+  console.error('Usage: node chicken.js PROGRAM [INPUT]');
+  console.error('');
+  console.error('Arguments:');
+  console.error('    PROGRAM: a path to a chicken program.');
+  console.error('    INPUT:   a string representing the input of the program in ASCII form.');
+  console.error('');
+}
